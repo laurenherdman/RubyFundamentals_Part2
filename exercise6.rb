dc276ca9117d1c5131da1dbf9265dd61331c191b
Lauren <lemherdman@hotmail.com>
2015-10-27 15:08:28 -0400

    Create print method

--- exercise6.rb
+++ exercise6.rb
@@ -2,9 +2,28 @@
 #Ruby Fundamentals Part 2
 #Exercise 6
 
-["carrots", "milk", "bananas", "oranges"].each do |grocery_list|
-puts "* #{grocery_list}"	
+
+# ["carrots", "milk", "bananas", "oranges"].each do |grocery_list|
+# 	puts "* #{grocery_list}"	
+# end
+
+# grocery_list.push(rice)
+
+# def print_list(list)
+# 	puts "* #{list}"
+# end
+
+# print_list(grocery_list)
+
+def print_list(list)
+	list.each {|list_item| puts "* #{list_item}"}
 end
 
 
+
+grocery_list = ["carrots", "milk", "bananas", "oranges"]
+print_list(grocery_list)
+puts " "
+grocery_list.push("rice")
+print_list(grocery_list)
 
