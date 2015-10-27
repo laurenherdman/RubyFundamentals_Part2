307a7a7500a79fbda71b782a9f09323285747a83
Lauren <lemherdman@hotmail.com>
2015-10-27 17:44:09 -0400

    Create check for banana and print second item

--- exercise6.rb
+++ exercise6.rb
@@ -21,9 +21,26 @@ end
 
 
 
+
+
 grocery_list = ["carrots", "milk", "bananas", "oranges"]
 print_list(grocery_list)
+
 puts " "
 grocery_list.push("rice")
 print_list(grocery_list)
+
+puts " "
+puts "There is #{grocery_list.length} items."
+
+puts " "
+
+if grocery_list.include?("bananas")
+	puts "You need to pick up bananas."
+else 
+	puts "You don't need to pick up bananas today."
+end
+
+puts " "
+puts grocery_list[1]
 
